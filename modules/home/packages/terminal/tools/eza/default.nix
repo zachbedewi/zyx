0ef6fdb79da332e9cef2a34b616578595745f658
Zach Bedewi <zachary.bedewi@protonmail.com>
2025-06-02 19:36:34 -0500

    Adding Eza configuration

---
+++ modules/home/packages/terminal/tools/eza/default.nix
@@ -0,0 +1,17 @@
+{
+  config = {
+    programs.eza = {
+      enable = true;
+      icons = "auto";
+      git = true;
+      enableZshIntegration = false;
+      extraOptions = [
+      	"--oneline"
+	"--long"
+	"--all"
+        "--group-directories-first"
+        "--header"
+      ];
+    };
+  };
+}
