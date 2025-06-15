3c7f73f5ede5b914aa63a35b85d935e499589fb5
Zach Bedewi <zachary.bedewi@protonmail.com>
2025-06-14 19:47:49 -0500

    Adding configuration for sudo

---
+++ modules/system/nixos/security/sudo.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkDefault mkForce;
+  inherit (lib.meta) getExe';
+in {
+  security = {
+    sudo-rs.enable = mkForce false;
+
+    sudo = {
+      enable = true;
+
+      execWheelOnly = mkForce true;
+
+      extraConfig = ''
+        Defaults lecture = never # rollback results in sudo lectures after each reboot
+	Defaults pwfeedback # Make typed password visible as asterisks
+	Defaults env_keep += "EDITOR PATH DISPLAY" # variables that will be passed to the root account
+	Defaults timestamp_timeout = 300 # Makes sudo ask for password every 5 minutes
+      '';
+    };
+  };
+}
