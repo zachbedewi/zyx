78ea0e29fa6a4e3d9c0848f38fc6010194beabe8
Zach Bedewi <zachary.bedewi@protonmail.com>
2025-08-17 13:05:05 -0500

    Added function to get all home manager configuration metadata

--- lib/filesystem/default.nix
+++ lib/filesystem/default.nix
@@ -30,6 +30,41 @@
       inherit system hostname;
       path = systemConfigurationPath + "/${hostname}";
     });
+
+  # Generates home-manager configuration metadata for a single system.
+  # Ex:
+  # {
+  #   "skitzo@eye-of-god" = {
+  #     system = "x86_64-linux";
+  #     hostname = "eye-of-god";
+  #     username = "skitzo";
+  #     path = /dev/zyx/homes/x86_64-linux/skitzo@eye-of-god;
+  #   };
+  #   "zach@eye-of-god" = {
+  #     system = "x86_64-linux";
+  #     hostname = "eye-of-god";
+  #     username = "zach";
+  #     path = /dev/zyx/homes/x86_64-linux/zach@eye-of-god;
+  #   };
+  # }
+  generateHomeConfigurationMetadataForSystem = homesPath: system: let
+    systemHomesPath = homesPath + "/${system}";
+    homeConfigs = getDirectoryNames systemHomesPath;
+    parseUserHost = userHost: let
+      parts = builtins.split "@" userHost;
+      username = builtins.elemAt parts 0;
+      hostname = builtins.elemAt parts 2;
+    in {
+      inherit username hostname;
+    };
+  in
+    genAttrs homeConfigs (userHost: let
+      parsed = parseUserHost userHost;
+    in {
+      inherit system;
+      inherit (parsed) username hostname;
+      path = systemHomesPath + "/${userHost}";
+    });
 in {
   inherit getDirectoryNames;
 
@@ -37,6 +72,11 @@ in {
     architectures = getDirectoryNames systemsPath;
   in
     foldl' (acc: system: acc // generateConfigurationMetadataForSystem systemsPath system) {} architectures;
+
+  genAllHomeConfigMetadata = homesPath: let
+    architectures = getDirectoryNames homesPath;
+  in
+    foldl' (acc: system: acc // generateHomeConfigurationMetadataForSystem homesPath system) {} architectures;
 
   filterNixosConfigurations = systems:
     filterAttrs (
